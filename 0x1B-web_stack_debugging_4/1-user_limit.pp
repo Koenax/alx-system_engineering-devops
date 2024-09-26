5a56ce5cb44eb387f6eb6ac97207741a5739b42d
Koenax <makoenamokoena5@gmail.com>
2024-09-26 21:02:49 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,16 @@
 # Reconfigure the OS for 'holberton' to login and open a file without any error message
 
+# Increase hard file limit for holberton user
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  command     => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  path        => '/usr/local/bin/:/bin/',
+  refreshonly => true,
 }
 
+# Increase soft file limit for holberton user
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  command     => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+  path        => '/usr/local/bin/:/bin/',
+  refreshonly => true,
 }
-
 
